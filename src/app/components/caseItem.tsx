38274a23b8080a34a1e1ce4935efcec68209f892
Marcelo Frohlich <96299094+MarceloFrohlich@users.noreply.github.com>
2024-06-05 17:52:45 -0300

    05/06

--- src/app/components/caseItem.tsx
+++ src/app/components/caseItem.tsx
@@ -1,6 +1,6 @@
 const CaseItem: React.FC = () => {
     return (
-        <section className="min-w-[30%] h-52 m-3 shadow-lg">
+        <section className="min-w-[30%] h-52 m-3 shadow-lg shadow-gray-900">
             <div className="bg-firstCase bg-cover bg-center h-full flex flex-col justify-end">
                 <div className="w-full bg-white/30 h-12 flex justify-center items-center">
                     <h1 className="text-gray-200">texto</h1>
--- src/app/pages/cases/page.tsx
+++ src/app/pages/cases/page.tsx
@@ -2,9 +2,9 @@ import CaseItem from "../../components/caseItem"
 
 const Cases: React.FC = () => {
     return (
-        <section className="bg-gray-200 w-full flex flex-col items-center pt-20 text-slate-800">
+        <section className="bg-gray-200 w-full flex flex-col items-center pt-20 text-slate-800 min-h-screen">
             <div>
-                <h1 className="text-3xl text-center mb-6 font-semibold">Cases</h1>
+                <h1 className="text-3xl text-center mb-2 font-semibold">Cases</h1>
             </div>
 
             <div className="w-full flex justify-center">
